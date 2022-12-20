704b62001ba8922e58f18ed5bd3f0667ddbf100c
thebonottox <theb.shopping@gmail.com>
2022-12-20 15:49:48 +0000

    Updating Progress up to fetchCatPics

--- challenges/1-cat-server.js
+++ challenges/1-cat-server.js
@@ -12,13 +12,50 @@ function fetchBannerContent(callback) {
   })
 }
 
-function fetchAllOwners() { }
+function fetchAllOwners(callback) { 
+  request('/owners', (err, arr) => {
+    const newArr = arr.map((item) => 
+      item.toLowerCase()
+    )
+    console.log(newArr)
+    callback(null, newArr)
+})
+}
+function fetchCatsByOwner(owner, callback) {
+  request(`/owners/${owner}/cats`, callback)
+}
 
-function fetchCatsByOwner() { }
+function fetchCatPics(catPicsArr, callback) {
+  const newArr = []
+  let callCount = 0;
+  if (catPicsArr.length === 0) callback(null) 
 
-function fetchCatPics() { }
+  catPicsArr.forEach((item) =>{
+    request(`/pics/${item}`, (err, data) => {
+     
+    if (err) {
+      newArr.push("placeholder.jpg") 
+    callCount++
+    }
+  
+    else {
+      newArr.push(data)
+      callCount++;
+    } 
+    if (callCount === catPicsArr.length) {
+      console.log(newArr);
+    callback(err, newArr);
+    }  
 
-function fetchAllCats() { }
+    })
+    
+  })
+  }
+ 
+
+function fetchAllCats() { 
+  
+}
 
 function fetchOwnersWithCats() { }
 
